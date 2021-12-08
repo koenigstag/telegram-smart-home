842f1f775ee9de54c48a6dc2de6160b013462051
Yevhenii Lapshynov <koenigstag@gmail.com>
2021-12-08 23:09:01 +0200

    Add freeman bind to specific user id

--- index.js
+++ index.js
@@ -18,48 +18,53 @@ function callback(err, obj) {
 slimbot.on('message', async message => {
   console.log(`New msg - ${message.text}\nHandling...\n`)
 
-  if (/freeman .*/.test(message.text)) {
-    if (message.chat.id !== 672406205) {
-      console.log(message)
-      console.log('\nMsg not handled..')
-      return
-    }
+  let responseText = '';
+  if (message.chat.id === 672406205 && /freeman .*/.test(message.text)) {
     const command = message.text.replace('freeman ', '')
     const result = shell.exec(command)
     const stderr = result.stderr
     const stdout = result.stdout
-    slimbot.sendMessage(message.chat.id, '**Stdout:**\n' + stdout + "\n\n" +"**Stderr:**\n" + stderr)
+    responseText = '**Stdout:**\n' + stdout + '\n\n' +'**Stderr:**\n' + stderr
   }
   else {
   switch (message.text) {
     case 'battery-status': {
       //await clipboard.write('battery-status')
       const batStat = JSON.parse(shell.exec('termux-battery-status').stdout)
-      slimbot.sendMessage(message.chat.id, `Percentage: ${batStat.percentage}. Status: ${batStat.status}`)
+      responseText = `Percentage: ${batStat.percentage}. Status: ${batStat.status}`
       break
     }
     case 'get-ip': {
       const ip = await axios.get('https://api.ipify.org')
       //console.log(ip)
-      slimbot.sendMessage(message.chat.id, ip.data)
+      responseText = ip.data
       break
     }
     case 'get-ip-lan': {
       const ip = shell.exec(`ifconfig wlan0 | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n 1`).stdout
       console.log(ip)
-      slimbot.sendMessage(message.chat.id, ip)
+      responseText = ip
       break
     }
     default: {
-      slimbot.sendMessage(
-        message.chat.id,
-       'Message received',
-       callback
-      );
+      responseText = 'Message received'
       break
     }
   }
   }
+
+  //slimbot.sendMessage(message.chat.id, responseText, callback)
+
+  slimbot.sendMessage(message.chat.id, responseText, {
+    reply_markup: JSON.stringify({
+      keyboard: [[
+        {text: 'freeman echo Vibrate && termux-vibrate '},
+        {text: 'freeman echo Notif sound && termux-media-player play ~/notif.mp3 '},
+        {text: 'freeman echo TTS && termux-tts-speak "хихи хихи"'},
+
+      ]]
+    })
+  })
 
   console.log('\nMsg handled..')
 });
